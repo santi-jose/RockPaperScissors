b92a2d9da7052fd20ab461c0b6d0737b66a10f18
jlsantia21 <jlsantia21@gmail.com>
2023-10-31 01:17:05 -0700

    Completed full implementation of Rock Paper Scissors. May need more testing and more editing.

--- script.js
+++ script.js
@@ -1,61 +1,5 @@
 
-// getComputerChoice
-// randomly return either Rock, Paper, or Scissors
-const getComputerChoice = () => {
-    // Math.random() returns random number betwee 0 (inclusive) and 1 (exclusive)
-    // Math.random() * 3 -> 0 (inclusive) to 3 (exclusive)
-    // Math.floor(Math.random * 3) -> get whole numbers from 0 to 2 (inclusive)
-    // Math.floor(Math.random() * 3) + 1 -> 1 to 3 (inclusive) whole numbers
-    let hand = Math.floor((Math.random() * 3)) + 1;
-    switch(hand){
-        case 1:
-            console.log(`computer hand: rock`);
-            return "rock";
 
-        case 2:
-            console.log(`computer hand: paper`);
-            return "paper";
-
-        case 3:
-            console.log(`computer hand: scissors`);
-            return "scissors";
-
-        default:
-            console.log("Something went wrong inside getComputerChoice() switch statement.");
-    }
-}
-
-const playRound = (playerSelection, computerSelection) =>{
-    playerSelection = playerSelection.toLowerCase(); // set player selection to lower case
-    if(playerSelection === "rock"){ // player selected rock
-        if(computerSelection === "rock"){ // computer picked rock
-            return "It's a draw! Rock vs Rock"; // player draw
-        }else if(computerSelection === "paper"){ // computer picked paper
-            return "You lose! Paper beats Rock!"; // player loss
-        }else{ // computerSelection === scissors; computer picked scissors
-            return "You win! Rock beats Scissors!"; // player win
-        }
-    }else if(playerSelection === "paper"){ // player selected paper
-        if(computerSelection === "rock"){ // computer picked rock
-            return "You win! Paper beats Rock!"; // player win
-        }else if(computerSelection === "paper"){  // computer picked paper
-            return "It's a draw! Paper vs Paper"; // player draw
-        }else{ // computerSelection === scissors; computer picked scissors
-            return "You lose! Scissors beats Paper!"; // player loss
-        }
-    }else if(playerSelection === "scissors"){ // player selected scissors
-        if(computerSelection === "rock"){ // computer picked rock
-            return "You lose! Rock beats Scissors!"; // player lose
-        }else if(computerSelection === "paper"){ // computer picked paper
-            return "You win! Scissors beats Paper!"; // player win
-        }else{ // computerSelection === scissors; computer picked scissors
-            return "It's a draw! Scissors vs Scissors"; // player draw
-        }
-    }else{ // input was invalid
-        return "Invalid input!";
-    }
-
-}
 
 // test all possible combinations
 // console.log(playRound("ROCK", "rock"));
@@ -70,4 +14,88 @@ const playRound = (playerSelection, computerSelection) =>{
 // console.log(playRound("SCISSORS", "paper"));
 // console.log(playRound("SCISSORS", "scissors"));
 
+const game = () => {
+    // initialize player and computer scores
+    let playerScore = 0;
+    let computerScore = 0;
 
+    // getComputerChoice
+    // randomly return either Rock, Paper, or Scissors
+    const getComputerChoice = () => {
+        // Math.random() returns random number betwee 0 (inclusive) and 1 (exclusive)
+        // Math.random() * 3 -> 0 (inclusive) to 3 (exclusive)
+        // Math.floor(Math.random * 3) -> get whole numbers from 0 to 2 (inclusive)
+        // Math.floor(Math.random() * 3) + 1 -> 1 to 3 (inclusive) whole numbers
+        let hand = Math.floor((Math.random() * 3)) + 1;
+        switch (hand) {
+            case 1:
+                // console.log(`computer hand: rock`);
+                return "rock";
+
+            case 2:
+                // console.log(`computer hand: paper`);
+                return "paper";
+
+            case 3:
+                // console.log(`computer hand: scissors`);
+                return "scissors";
+
+            default:
+                console.log("Something went wrong inside getComputerChoice() switch statement.");
+        }
+    }
+
+    const playRound = (playerSelection, computerSelection) => {
+        playerSelection = playerSelection.toLowerCase(); // set player selection to lower case
+        if (playerSelection === "rock") { // player selected rock
+            if (computerSelection === "rock") { // computer picked rock
+                return "It's a draw! Rock vs Rock"; // player draw
+            } else if (computerSelection === "paper") { // computer picked paper
+                computerScore++; // increment computerScore
+                return "You lose! Paper beats Rock!"; // player loss
+            } else { // computerSelection === scissors; computer picked scissors
+                playerScore++;
+                return "You win! Rock beats Scissors!"; // player win
+            }
+        } else if (playerSelection === "paper") { // player selected paper
+            if (computerSelection === "rock") { // computer picked rock
+                playerScore++;
+                return "You win! Paper beats Rock!"; // player win
+            } else if (computerSelection === "paper") {  // computer picked paper
+                return "It's a draw! Paper vs Paper"; // player draw
+            } else { // computerSelection === scissors; computer picked scissors
+                computerScore++;
+                return "You lose! Scissors beats Paper!"; // player loss
+            }
+        } else if (playerSelection === "scissors") { // player selected scissors
+            if (computerSelection === "rock") { // computer picked rock
+                computerScore++;
+                return "You lose! Rock beats Scissors!"; // player lose
+            } else if (computerSelection === "paper") { // computer picked paper
+                playerScore++;
+                return "You win! Scissors beats Paper!"; // player win
+            } else { // computerSelection === scissors; computer picked scissors
+                return "It's a draw! Scissors vs Scissors"; // player draw
+            }
+        } else { // input was invalid
+            return "Invalid input!";
+        }
+
+    }
+
+    // play 5 rounds
+    for (let i = 0; i < 5; i++) {
+        console.log(playRound(prompt("Enter your hand!"), getComputerChoice()));
+        console.log(`playerScore: ${playerScore}, computerScore: ${computerScore}`);
+    }
+
+    if(playerScore > computerScore){
+        console.log(`Player wins! Player: ${playerScore}, Bot: ${computerScore}`);
+    }else if(playerScore < computerScore){
+        console.log(`Player loses! Player: ${playerScore}, Bot: ${computerScore}`);
+    }else{
+        console.log(`Draw! Player: ${playerScore}, Bot: ${computerScore}`);
+    }
+}
+
+game();
